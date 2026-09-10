32cd4285fc8d80c8530439d3236ba05fd5c921b1
DimasRabelo <dimas.rabelosouza@gmail.com>
2026-09-10 20:24:11 -0300

    Initial commit

--- teste_sistema.py
+++
@@ -1,9 +0,0 @@
-from funcoes import adicionar_produto, listar_produtos
-
-# Testando adicionar uma cerveja
-adicionar_produto("Cerveja Skol Lata", 50, 2.50, 4.50)
-
-# Verificando se ela aparece na lista
-print("Produtos no estoque:")
-for p in listar_produtos():
-    print(p)
